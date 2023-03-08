b6afb991faffe8f4751bb301cd4bff015808e8dc
Bennett <banorman@umich.edu>
2023-03-08 16:12:23 -0500

    removed login api

--- recipe-manager-backend/app.py
+++ recipe-manager-backend/app.py
@@ -5,12 +5,3 @@ app = Flask(__name__)
 @app.route("/api/hello")
 def hello_world():
     return jsonify("hello")
-
-@app.route("/api/login", methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        print(f"Username: {request.form['username']}")
-        print(f"Password: {request.form['password']}")
-        return {
-            "msg": "success"
-        }
