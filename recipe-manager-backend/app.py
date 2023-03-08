31dd6e95b8515a83cb64580738e1aaa579a54230
Bennett <banorman@umich.edu>
2023-03-08 16:13:46 -0500

    updated

--- recipe-manager-backend/app.py
+++ recipe-manager-backend/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+import sqlite3
+import json
 
 app = Flask(__name__)
 
-@app.route("/api/hello")
-def hello_world():
-    return jsonify("hello")
+def get_db_connection():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+# Example GET route for /recipe/<recipe_id>
+@app.route("/recipe/<recipe_id>")
+def get_recipe(recipe_id):
+    connection = get_db_connection()
+    test_recipe = connection.execute('SELECT * FROM Recipe WHERE recipe_id = ?', (recipe_id,)).fetchall()
+    connection.close()
+    return json.dumps([dict(i) for i in test_recipe])
