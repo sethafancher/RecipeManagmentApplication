d9522036ee00f8b9c7b2552ce6d06c50b8b00481
Seth Fancher <sethafancher@gmail.com>
2023-04-15 17:52:22 -0400

    Make dummy DB prettier

--- recipe-manager-backend/init_db.py
+++ recipe-manager-backend/init_db.py
@@ -11,36 +11,44 @@ cur.execute("INSERT INTO User (user_id, first_name, last_name, username, passwor
             "VALUES (1, 'Seth', 'Fancher', 'sfancher', 'password');")
 
 cur.execute("INSERT INTO Recipe (title, creator, description)" +
-            "VALUES ('Chicken', 1, 'My chicken recipe');")
+            "VALUES ('Montreal Chicken', 1, 'A deliciously moist chicken breast recipe');")
 cur.execute("INSERT INTO Recipe (title, creator, description)" +
-            "VALUES ('Steak', 1, 'My steak recipe');")
+            "VALUES ('New York Strip Steak', 1, 'The perfect way to cook a New York Strip steak');")
 
 cur.execute("INSERT INTO Recipe_Ingredient (ingredient_id, recipe_id, name, description, amount, unit)" +
-            "VALUES (1, 1, 'Sauce', 'Red sauce', 4, 'cups');")
+            "VALUES (1, 1, 'chicken breast', '', 2, 'lbs');")
 cur.execute("INSERT INTO Recipe_Ingredient (ingredient_id, recipe_id, name, description, amount, unit)" +
-            "VALUES (2, 1, 'Seasoning', 'Montreal chicken', 2, 'dashes');")
+            "VALUES (2, 1, 'Montreal Chicken seasoning', '', 1, 'cup');")
 cur.execute("INSERT INTO Recipe_Ingredient (ingredient_id, recipe_id, name, description, amount, unit)" +
-            "VALUES (1, 2, 'Sauce', 'White sauce', 3, 'cups');")
+            "VALUES (3, 1, 'salted butter', 'Montreal chicken', 2, 'tbsp');")
 cur.execute("INSERT INTO Recipe_Ingredient (ingredient_id, recipe_id, name, description, amount, unit)" +
-            "VALUES (2, 2, 'Seasoning', 'Montreal steak', 6, 'dashes');")
+            "VALUES (1, 2, 'New York Strip steak', '', 12, 'oz');")
+cur.execute("INSERT INTO Recipe_Ingredient (ingredient_id, recipe_id, name, description, amount, unit)" +
+            "VALUES (2, 2, 'salt', '', 6, 'dashes');")
+cur.execute("INSERT INTO Recipe_Ingredient (ingredient_id, recipe_id, name, description, amount, unit)" +
+            "VALUES (3, 2, 'pepper', '', 4, 'dashes');")
 
 cur.execute("INSERT INTO Recipe_Step (step_id, recipe_id, title, description)" +
-            "VALUES (1, 1, 'Prepare chicken', 'This is the first step');")
+            "VALUES (1, 1, 'Rinse chicken', 'Rinse the chicken breasts, making sure that they are patted dry afterwards');")
 cur.execute("INSERT INTO Recipe_Step (step_id, recipe_id, title, description)" +
-            "VALUES (2, 1, 'Cook chicken', 'This is the second step');")
+            "VALUES (2, 1, 'Prepare chicken', 'Place the chicken on the baking sheet, and add the seasoning and butter');")
 cur.execute("INSERT INTO Recipe_Step (step_id, recipe_id, title, description)" +
-            "VALUES (1, 2, 'Prepare steak', 'This is the first step');")
+            "VALUES (3, 1, 'Bake chicken', 'Bake the chicken in a conventional oven for 20 minutes at 425 degrees F, then eat!');")
 cur.execute("INSERT INTO Recipe_Step (step_id, recipe_id, title, description)" +
-            "VALUES (2, 2, 'Cook steak', 'This is the second step');")
+            "VALUES (1, 2, 'Season steak', 'Liberally season both sides of the steak with salt and pepper');")
+cur.execute("INSERT INTO Recipe_Step (step_id, recipe_id, title, description)" +
+            "VALUES (2, 2, 'Sear steak', 'Sear the steak on a cast iron pan for 3 minutes on each side');")
+cur.execute("INSERT INTO Recipe_Step (step_id, recipe_id, title, description)" +
+            "VALUES (3, 2, 'Bake steak', 'Bake the steak in a conventional oven for 6 minutes at 500 degrees F');")
+cur.execute("INSERT INTO Recipe_Step (step_id, recipe_id, title, description)" +
+            "VALUES (4, 2, 'Cover steak', 'Cover the steak with aluminum foil for 5 minutes, then eat!');")
 
 cur.execute("INSERT INTO Recipe_Equipment (equipment_id, recipe_id, name, description)" +
-            "VALUES (1, 1, 'Plate', 'This is a plate');")
+            "VALUES (1, 1, 'Baking sheet', '');")
 cur.execute("INSERT INTO Recipe_Equipment (equipment_id, recipe_id, name, description)" +
-            "VALUES (2, 1, 'Knife', 'This is a knife');")
+            "VALUES (1, 2, 'Cast iron pan', '');")
 cur.execute("INSERT INTO Recipe_Equipment (equipment_id, recipe_id, name, description)" +
-            "VALUES (1, 2, 'Bowl', 'This is a bowl');")
-cur.execute("INSERT INTO Recipe_Equipment (equipment_id, recipe_id, name, description)" +
-            "VALUES (2, 2, 'Fork', 'This is a fork');")
+            "VALUES (2, 2, 'Aluminum foil', '');")
 
 
 connection.commit()
