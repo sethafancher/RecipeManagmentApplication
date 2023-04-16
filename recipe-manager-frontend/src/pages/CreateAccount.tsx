e8f55cd33801da8da557188d829a144402eaa30d
Bennett <banorman@umich.edu>
2023-04-16 14:51:33 -0400

    check box for steps now starts as unchecked

--- recipe-manager-frontend/src/pages/CreateAccount.tsx
+++ recipe-manager-frontend/src/pages/CreateAccount.tsx
@@ -14,6 +14,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createUser } from "../RecipeManagerClient";
 
 const theme = createTheme();
 
